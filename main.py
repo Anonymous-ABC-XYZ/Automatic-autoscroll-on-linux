b295b79b76f120ce15c8341b3e7611256f266a52
ABC <70518092+Anonymous-ABC-XYZ@users.noreply.github.com>
2023-06-08 17:42:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import os
+import re
 
 final_numbers = []
 all_ids = os.popen('xinput| grep -i Mouse').read()
 
 id_number = all_ids.split()
-print(id_number)
 
 correct_id_number = []
 
 for n in id_number:
     if "id" in n:
         correct_id_number.append(n)
+for n in correct_id_number:
+    final_numbers += re.findall(r'\d+', n)
 
-
-final_numbers.append(int(correct_id_number[0][3]))
-final_numbers.append(int(correct_id_number[1][3]))
-
-os.popen(f'xinput set-prop {final_numbers[0]} "libinput Scroll Method Enabled" 0, 0, 1')
-os.popen(f'xinput set-prop {final_numbers[0]} "libinput Button Scrolling Button" 2')
-os.popen(f'xinput set-prop {final_numbers[1]} "libinput Scroll Method Enabled" 0, 0, 1')
-os.popen(f'xinput set-prop {final_numbers[1]} "libinput Button Scrolling Button" 2')
+for n in final_numbers:
+    os.popen(f'xinput set-prop {int(n)} "libinput Scroll Method Enabled" 0, 0, 1')
+    os.popen(f'xinput set-prop {int(n)} "libinput Button Scrolling Button" 2')
